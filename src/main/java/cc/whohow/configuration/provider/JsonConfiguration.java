707486739d3e8b348b78da9c513e72c9e69e9f40
huhao <415328529@qq.com>
2018-06-13 18:37:43 +0800

    优化Json、Yaml配置

--- src/main/java/cc/whohow/configuration/provider/JsonConfiguration.java
+++ src/main/java/cc/whohow/configuration/provider/JsonConfiguration.java
@@ -1,6 +1,8 @@
 package cc.whohow.configuration.provider;
 
 import cc.whohow.configuration.FileBasedConfigurationSource;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.InputStream;
@@ -9,13 +11,37 @@ public class JsonConfiguration<T> extends AbstractFileBasedConfiguration<T> {
     private static final ObjectMapper DEFAULT_OBJECT_MAPPER = new ObjectMapper();
 
     private final ObjectMapper objectMapper;
-    private final Class<T> type;
+    private final JavaType type;
 
     public JsonConfiguration(FileBasedConfigurationSource configurationSource, Class<T> type) {
         this(configurationSource, type, DEFAULT_OBJECT_MAPPER);
     }
 
+    public JsonConfiguration(FileBasedConfigurationSource configurationSource, TypeReference<T> type) {
+        this(configurationSource, type, DEFAULT_OBJECT_MAPPER);
+    }
+
+    public JsonConfiguration(FileBasedConfigurationSource configurationSource, String typeCanonicalName) {
+        this(configurationSource, typeCanonicalName, DEFAULT_OBJECT_MAPPER);
+    }
+
+    public JsonConfiguration(FileBasedConfigurationSource configurationSource, JavaType type) {
+        this(configurationSource, type, DEFAULT_OBJECT_MAPPER);
+    }
+
     public JsonConfiguration(FileBasedConfigurationSource configurationSource, Class<T> type, ObjectMapper objectMapper) {
+        this(configurationSource, objectMapper.getTypeFactory().constructType(type), objectMapper);
+    }
+
+    public JsonConfiguration(FileBasedConfigurationSource configurationSource, TypeReference<T> type, ObjectMapper objectMapper) {
+        this(configurationSource, objectMapper.getTypeFactory().constructType(type), objectMapper);
+    }
+
+    public JsonConfiguration(FileBasedConfigurationSource configurationSource, String typeCanonicalName, ObjectMapper objectMapper) {
+        this(configurationSource, objectMapper.getTypeFactory().constructFromCanonical(typeCanonicalName), objectMapper);
+    }
+
+    public JsonConfiguration(FileBasedConfigurationSource configurationSource, JavaType type, ObjectMapper objectMapper) {
         super(configurationSource);
         this.objectMapper = objectMapper;
         this.type = type;
--- src/main/java/cc/whohow/configuration/provider/YamlConfiguration.java
+++ src/main/java/cc/whohow/configuration/provider/YamlConfiguration.java
@@ -1,6 +1,8 @@
 package cc.whohow.configuration.provider;
 
 import cc.whohow.configuration.FileBasedConfigurationSource;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
@@ -11,7 +13,31 @@ public class YamlConfiguration<T> extends JsonConfiguration<T> {
         this(configurationSource, type, DEFAULT_YAML_OBJECT_MAPPER);
     }
 
+    public YamlConfiguration(FileBasedConfigurationSource configurationSource, TypeReference<T> type) {
+        this(configurationSource, type, DEFAULT_YAML_OBJECT_MAPPER);
+    }
+
+    public YamlConfiguration(FileBasedConfigurationSource configurationSource, String typeCanonicalName) {
+        this(configurationSource, typeCanonicalName, DEFAULT_YAML_OBJECT_MAPPER);
+    }
+
+    public YamlConfiguration(FileBasedConfigurationSource configurationSource, JavaType type) {
+        this(configurationSource, type, DEFAULT_YAML_OBJECT_MAPPER);
+    }
+
     public YamlConfiguration(FileBasedConfigurationSource configurationSource, Class<T> type, ObjectMapper objectMapper) {
+        super(configurationSource, type, objectMapper);
+    }
+
+    public YamlConfiguration(FileBasedConfigurationSource configurationSource, TypeReference<T> type, ObjectMapper objectMapper) {
+        super(configurationSource, type, objectMapper);
+    }
+
+    public YamlConfiguration(FileBasedConfigurationSource configurationSource, String typeCanonicalName, ObjectMapper objectMapper) {
+        super(configurationSource, typeCanonicalName, objectMapper);
+    }
+
+    public YamlConfiguration(FileBasedConfigurationSource configurationSource, JavaType type, ObjectMapper objectMapper) {
         super(configurationSource, type, objectMapper);
     }
 }
